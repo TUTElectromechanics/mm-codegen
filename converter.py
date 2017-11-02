965378076ea92e42819741e5c27e57d777301b87
Juha Jeronen <juha.jeronen@jyu.fi>
2017-11-02 12:01:25 +0200

    converter.py: move utility functions to a separate module

--- converter.py
+++ converter.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Convert B-spline basis functions to cubic-spline interpolated data in Elmer format.
 
-import sys
-import math
+The same approach works with very little modification for any set of basis functions.
+"""
+# -JJ 2017-10-18
 
 import numpy as np
 
@@ -15,96 +17,7 @@ import bspline.splinelab as splinelab
 
 import elmerspline
 
-#def round_sig(x, sig=2, method='round'):
-#    """Round float x to sig significant figures.
-#
-#    method = string:
-#      'round': round by standard rounding rules (default)
-#      'ceil':  always round up
-#      'floor': always round down
-#
-#    See
-#    http://stackoverflow.com/questions/3410976/how-to-round-a-number-to-significant-figures-in-python"""
-##    # As per answer by indgar, and adding abs() as suggested by dgorissen to handle also x<0.
-##    # Added also handling for zero as a special case.
-##    #
-##    if x == 0.0:
-##        return x
-##    else:
-##        return round(x, sig-int(math.floor(math.log10(abs(x))))-1)
-#
-#    # Chris Stringfellow's answer (which allows for generalization to "ceil"/"floor" also):
-#    #
-#    # To round an integer to 1 significant figure the basic idea is to convert it to a floating point
-#    # with 1 digit before the point and round that, then convert it back to its original integer size.
-#    #
-#    # To do this we need to know the largest power of 10 less than the integer. We can use floor of
-#    # the log 10 function for this. 
-#    #
-#    #from math import log10, floor
-#    #def round_int(i,places):
-#    #    if i == 0:
-#    #        return 0
-#    #    isign = i/abs(i)
-#    #    i = abs(i)
-#    #    if i < 1:
-#    #        return 0
-#    #    max10exp = floor(log10(i))
-#    #    if max10exp+1 < places:
-#    #        return i
-#    #    sig10pow = 10**(max10exp-places+1)
-#    #    floated = i*1.0/sig10pow
-#    #    defloated = round(floated)*sig10pow
-#    #    return int(defloated*isign)
-#
-#    # Adapting the above for floats, and adding logic to round always up or down, we have:
-#
-#    if x == 0.0:
-#        return x
-#
-#    xsign = x/abs(x)
-#    x = abs(x)
-#
-#    if xsign > 0:
-#        if method == 'ceil':
-#            rounded = math.ceil
-#        elif method == 'floor':
-#            rounded = math.floor
-#        else: # method == 'round':
-#            rounded = round
-#    else:
-#        # emulate correct behaviour of ceil/floor for negative input
-#        # (given that our x is always positive at this point)
-#        if method == 'floor':
-#            rounded = math.ceil
-#        elif method == 'ceil':
-#            rounded = math.floor
-#        else: # method == 'round':
-#            rounded = round
-#
-#    max10exp = math.floor(math.log10(x))
-#    sig10pow = 10**(max10exp-sig+1)
-#    floated = x*1.0/sig10pow
-#    defloated = rounded(floated)*sig10pow
-#    return defloated*xsign
-
-def ulp(x):
-    """Unit in the last place for float x."""
-    # For the half-open interval [1,2), we have ulp = eps; scale from that.
-    #
-    # TODO: we assume x is normal (no support for subnormal numbers)
-    #
-    eps = sys.float_info.epsilon
-    x = abs(x)
-    fac = 2**math.floor(math.log2(x))
-    return fac*eps
-
-def quadspace(x1, x2, nx, refine='left'):
-    """Make a quadratic spacing with more points either on the left or on the right."""
-    xx = np.linspace(0, 1, nx)**2
-    if refine == 'right':
-        xx = (1 - xx)[::-1]
-    return x1 + (x2 - x1)*xx
+from util import ulp, quadspace
 
 def main():
     # Original knots
@@ -152,19 +65,6 @@ def main():
         scalar_fj = lambda x: scalar_f(x)[j]
         vector_fj = np.vectorize(scalar_fj)  # horrible performance, this is just a Python loop
         return vector_fj
-
-#    # equivalent, manually:
-#    def make_f(j):
-#        scalar_f = spl.diff(order=1)  # lambda x: ...
-#        def vector_f(x):
-#            if isinstance(x, np.ndarray):
-#                out = np.empty( np.shape(x), dtype=np.float64 )
-#                for k,xk in enumerate(x):
-#                    out[k] = scalar_f(xk)[j]
-#            else:
-#                out = scalar_f(x)[j]
-#            return out
-#        return vector_f
 
     for j in range(1, nb-1):
         # The maximum of the jth basis function is known to be near knot j,
@@ -223,7 +123,7 @@ def main():
         # Create Elmer-format cubic spline approximation using the data at the sites.
         rr = elmerspline.solve_coeffs(xx[j,:], ff[j,:])
 
-        # TODO: tabulate and save rr
+        # TODO: tabulate and save rr for use with Elmer
 
         # Plot the original basis function.
         plt.plot( xxvis, ffvis[j,:], linestyle='dashed', color=colors[j] )
